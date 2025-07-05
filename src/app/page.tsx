4c3d01afdb901618e5b78a922512ee371f87f27a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-07-05 19:37:39 -0300

    feat: add Header component with responsive design and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/app/ui/navbar/navbar";
+import Header from "@/app/ui/header/header";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Header />
       <div style={{ height: "250dvh" }} />
     </main>
   );
